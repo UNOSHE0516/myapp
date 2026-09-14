1fa374c9a3a9c5c0e31944d14b3d4968ba104c8a
UNOSHE0516 <hiro0516jms@gmail.com>
2026-09-14 18:52:31 +0900

    Taskクラス作成

--- src/main/java/com/Task.java
+++ src/main/java/com/Task.java
@@ -2,4 +2,37 @@ package com;
 
 public class Task {
 
+    private int number;
+    private String name;
+    private boolean completed; 
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+   
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+
+    public void setCompleted(boolean completed) {
+    this.completed = completed;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
 }
+
+
+
+
+
